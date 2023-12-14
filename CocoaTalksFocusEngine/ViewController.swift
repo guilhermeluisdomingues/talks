e925afa6ba83f4b99ced7a5653e382bc0454bc0a
Guilherme Domingues <inf00312@sky.com.br>
2023-12-14 19:06:18 -0300

    Initial Commit

---
+++ CocoaTalksFocusEngine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocoaTalksFocusEngine
+//
+//  Created by Guilherme Domingues on 14/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
